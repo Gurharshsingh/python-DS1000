5699f725d0f626147b3a16bfd6739b25c8824be0
Gurharsh_002 <ggurharsh@gmail.com>
2026-06-18 16:13:46 +0530

    add

---
+++ 3. ifelsefor.py
@@ -0,0 +1,85 @@
+print("=============if elif else ==============")
+
+# age = int(input("Enter Your age:"))
+
+# if age>=18:
+#     print("you can drive")
+
+# else:
+#     print("you cannot drive")
+
+# marks = int(input("Enter Your marks:"))
+
+
+# if marks>=90 and marks<=100:
+#     print("grade A")
+
+# elif marks>=80 and marks<=89:
+#     print("grade B")
+
+# elif marks>=70 and marks<=79:
+#     print("grade C")
+
+# elif marks<=69 and marks>=0:
+#     print("Fail")
+
+# else:
+#     print("invalid input")
+
+
+#odd even
+
+# num = int(input("Enter a number:"))
+
+# if num%2 ==0:
+#     print("even")
+
+# else:
+#     print("odd")
+
+print("==============for loop=============")
+
+
+for i in range(10):
+    print(i)
+
+
+for i in range(10):
+    print("Hello")
+
+for i in range(1,20):
+    print(i)
+
+for j in range(2,50):
+    print(j)
+
+for k in range(2,50,3):
+    print(k)
+
+for l in range(50,2,-2):
+    print(l)
+
+subjects = int(input("Enter number of subjects:"))
+
+
+for i in range(subjects):
+    marks = int(input("Enter Yor marks:"))
+
+
+    if marks>=90 and marks<=100:
+        print("grade A")
+
+    elif marks>=80 and marks<=89:
+        print("grade B")
+
+    elif marks>=70 and marks<=79:
+        print("grade C")
+
+    elif marks<=69 and marks>=0:
+        print("Fail")
+
+    else:
+        print("invalid input")
+
+
+
---
+++ 4. nestedif_else_for.py
@@ -0,0 +1,59 @@
+# marks = int(input("Enter Your marks:"))
+
+
+# if marks>=90 and marks<=100:
+#     if marks>=95 and marks <=100:
+#         print("Grade A+")
+#     else:
+#         print("Grade A")
+# elif marks>=80 and marks<=89:
+#     if marks>=85 and marks <=89:
+#         print("Grade B+")
+#     else:
+#         print("grade B")
+# elif marks>=70 and marks<=79:
+#     if marks>=75 and marks <=79:
+#         print("Grade C+")
+#     else:
+#         print("grade C")
+
+# elif marks<=69 and marks>=0:
+#     print("Fail")
+
+# else:
+#     print("invalid input")
+
+
+# subjects = int(input("Enter number of subjects:"))
+# for i in range(subjects):
+#     marks = int(input("Enter Your marks:"))
+
+
+#     if marks>=90 and marks<=100:
+#         if marks>=95 and marks <=100:
+#             print("Grade A+")
+#         else:
+#             print("Grade A")
+#     elif marks>=80 and marks<=89:
+#         if marks>=85 and marks <=89:
+#             print("Grade B+")
+#         else:
+#             print("grade B")
+#     elif marks>=70 and marks<=79:
+#         if marks>=75 and marks <=79:
+#             print("Grade C+")
+#         else:
+#             print("grade C")
+
+#     elif marks<=69 and marks>=0:
+#         print("Fail")
+
+#     else:
+#         print("invalid input")
+
+print("==========nested Loops=============")
+
+for i in range(1,6):
+    for j in range(0,i):
+        print("*", end=" ")
+    print()
